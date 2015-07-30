a725250d21963f907cc124a5d3014439b37f52fd
sasairc <sasairc@ssiserver.moe.hm>
2015-07-30 19:43:02 +0900

    細かなコード整形

--- wasawasa.c
+++ wasawasa.c
@@ -8,9 +8,9 @@
  * for more details.
  */
  
-#define MODNAME "wasawasa"
-#define CHAR_MAJOR      254
-#define CHAR_MINOR      1
+#define MODNAME     "wasawasa"
+#define CHAR_MAJOR  254
+#define CHAR_MINOR  1
 
 #include <linux/semaphore.h>
 #include <linux/kernel.h>
@@ -28,59 +28,60 @@ static struct semaphore cdev_sem;
 
 static int open_chardev(struct inode *inode, struct file *file)
 {
-        return 0;
+    return 0;
 }
 
 static int release_chardev(struct inode *inode, struct file *file)
 {
-        return 0;
+    return 0;
 }
 
 static ssize_t read_chardev(struct file *file, char *buf, size_t count, loff_t *offset)
 {
-        char *str = "わさわさ";
+    char *str = "わさわさ";
 
-        down_interruptible(&cdev_sem);
-        copy_to_user(buf, str, strlen(str));
+    down_interruptible(&cdev_sem);
+    copy_to_user(buf, str, strlen(str));
 
-        *offset += strlen(str);
-        up(&cdev_sem);
-        return strlen(str);
+    *offset += strlen(str);
+    up(&cdev_sem);
+
+    return strlen(str);
 }
 
 static struct file_operations cdev_fops = {
-        .owner  = THIS_MODULE,
-        .open   = open_chardev,
-        .read   = read_chardev,
-        .release = release_chardev,
+    .owner  = THIS_MODULE,
+    .open   = open_chardev,
+    .read   = read_chardev,
+    .release = release_chardev,
 };
 
 static int __init init_chardev(void)
 {
-        int ret;
-        dev_t dev;
+    int     ret;
+    dev_t   dev;
 
-        dev = MKDEV(CHAR_MAJOR, 0);
-        cdev_init(&cdev, &cdev_fops);
+    dev = MKDEV(CHAR_MAJOR, 0);
+    cdev_init(&cdev, &cdev_fops);
 
-        ret = cdev_add(&cdev, dev, CHAR_MINOR);
-        if (ret < 0) {
-                printk(KERN_WARNING "%s: cdev_add() failure.\n", MODNAME);
-                return ret;
-        }
+    ret = cdev_add(&cdev, dev, CHAR_MINOR);
+    if (ret < 0) {
+        printk(KERN_WARNING "%s: cdev_add() failure.\n", MODNAME);
+        return ret;
+    }
 
-        sema_init(&cdev_sem, 1);
+    sema_init(&cdev_sem, 1);
 
-        return 0;
+    return 0;
 }
 
 static void __exit exit_chardev(void)
 {
-        dev_t dev;
+    dev_t dev;
 
-        dev = MKDEV(CHAR_MAJOR, 0);
-        cdev_del(&cdev);
-        unregister_chrdev_region(dev, CHAR_MINOR);
+    dev = MKDEV(CHAR_MAJOR, 0);
+    cdev_del(&cdev);
+    unregister_chrdev_region(dev, CHAR_MINOR);
 }
 
 module_init(init_chardev);
